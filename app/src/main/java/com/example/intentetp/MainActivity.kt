cc91d93d07e8a4a1f53f5ad7d8bb21b79c804db9
arrchita <deepg9997@gmail.com>
2024-11-23 01:14:59 +0530

    add files

---
+++ app/src/main/java/com/example/intentetp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.intentetp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.intentetp.R
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+    }
+
+    fun search(view: android.view.View) {
+        val url = editText.text.toString()
+        if (url.isNotEmpty()) {
+            val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(urlIntent)
+        }
+        else {
+            editText.error = "Please enter a valid URL"
+        }
+    }
+
+}
